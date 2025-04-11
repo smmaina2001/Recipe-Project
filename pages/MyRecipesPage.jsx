e2e614f0c797103f562cc2d4a3b69e53d849c241
Steve <smmaina2001@gmail.com>
2025-04-11 12:48:35 +0300

    Initial project setup

---
+++ pages/MyRecipesPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyRecipesPage() {
+  return (
+    <div>
+      <h1>My Recipes</h1>
+      {/* Add logic to fetch and display user's recipes */}
+    </div>
+  );
+}
+
+export default MyRecipesPage;
