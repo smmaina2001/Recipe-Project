1ed0963fb61daee11433e8df343e0d3f9ae0b6e2
Larson Mwenda <larsonmwendah07@gmail.com>
2025-04-12 01:12:58 +0300

    base url change

--- api/recipes.js
+++ api/recipes.js
@@ -1,17 +1,17 @@
-const API_URL = 'http://localhost:5000';
+const BASE_URL = 'http://localhost:5555';
 
 export async function fetchRecipes() {
-  const res = await fetch(`${API_URL}/recipes`);
+  const res = await fetch(`${BASE_URL}/recipes`);
   return await res.json();
 }
 
 export async function fetchRecipeById(id) {
-  const res = await fetch(`${API_URL}/recipes/${id}`);
+  const res = await fetch(`${BASE_URL}/recipes/${id}`);
   return await res.json();
 }
 
 export async function createRecipe(recipeData, token) {
-  const res = await fetch(`${API_URL}/recipes`, {
+  const res = await fetch(`${BASE_URL}/recipes`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
